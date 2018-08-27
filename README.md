2700558cf49d7c9df6041f0f3e29eb54e7b470cf
Keith Kim <keith@keithkim.org>
2018-08-26 23:50:52 -0400

    Simple pom.xml and main

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pom
 A maven dependency adder/remover/updater
+
+## Building
+
+- mvn clean package
+
+## Running
+
+- java -jar target/pom-1.0-SNAPSHOT.jar
+
---
+++ src/main/java/org/keithkim/pom/Pom.java
@@ -0,0 +1,7 @@
+package org.keithkim.pom;
+
+public class Pom {
+    public static void main(String[] args) {
+        System.out.println("Hello, world.");
+    }
+}
